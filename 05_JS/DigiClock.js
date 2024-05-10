4b34c9fab4cdccfba9f1fb95a22b0b40286affd9
Pranav Pawar <ppawar2612@gmail.com>
2024-05-10 18:01:15 +0530

    Update DigiClock.js

--- 05_JS/DigiClock.js
+++ 05_JS/DigiClock.js
@@ -1,4 +1,24 @@
 const hourel=document.getElementById("hours");
 const minutesel=document.getElementById("minutes");
 const secondsel=document.getElementById("seconds");
-const amel=document.getElementById("AMPM")
+const amel=document.getElementById("AMPM");
+
+function time(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h>12){
+        h=h-12;
+        ampm="PM";
+    }
+    hourel.innerText=h;
+    amel.innerText=ampm;
+    minutesel.innerText=m;
+    secondsel.innerText=s;
+    setTimeout(()=>{
+        time();
+    },1000)
+}
+time();
