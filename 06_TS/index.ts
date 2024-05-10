5163919e020c835ddd70e38cf5e9daface179283
Pranav Pawar <ppawar2612@gmail.com>
2024-05-11 01:29:25 +0530

    Update index.ts

--- 06_TS/index.ts
+++ 06_TS/index.ts
@@ -1,9 +1,7 @@
-// Get elements from the DOM
 const numInput = document.getElementById("numInput") as HTMLInputElement;
 const printButton = document.getElementById("printButton") as HTMLButtonElement;
 const display = document.getElementById("display") as HTMLSpanElement;
 
-// Function to handle button click
 function printNumber(): void {
     const num: number = parseFloat(numInput.value); // Parse the input value to a number
 
@@ -14,5 +12,4 @@ function printNumber(): void {
     }
 }
 
-// Add event listener to the button
 printButton.addEventListener("click", printNumber);
