cc86932366e78494bb5e7f2c0ff68d8b1a9b042f
Pranav Pawar <ppawar2612@gmail.com>
2024-05-10 17:48:04 +0530

    .

---
+++ 05_JS/DigiClock.js
@@ -0,0 +1,4 @@
+const hourel=document.getElementById("hours");
+const minutesel=document.getElementById("minutes");
+const secondsel=document.getElementById("seconds");
+const amel=document.getElementById("AMPM")
